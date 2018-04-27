77e9a35ccbdb777e5b292e734c632175dac6f0c6
Simon Plourde <simon.plourde@gmail.com>
2018-04-27 10:41:47 -0400

    Release 1.2.0
    Signed-off-by: Simon Plourde <simon.plourde@gmail.com>

--- js/constants.js
+++ js/constants.js
@@ -7,7 +7,7 @@ constantModule.value('THEMES', [
 ]);
 
 // Version
-var version = '1.1.3';
+var version = '1.2.0';
 constantModule.constant('VERSION', version);
 constantModule.value('VERSIONS', {
   uchiwa: version
