addc75a25c672247ddac6d0f90a3650d0c1b399c
Justin Kolberg <amd.prophet@gmail.com>
2019-02-27 13:43:10 -0800

    Release 1.6.0
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- js/constants.js
+++ js/constants.js
@@ -7,7 +7,7 @@ constantModule.value('THEMES', [
 ]);
 
 // Version
-var version = '1.5.0';
+var version = '1.6.0';
 constantModule.constant('VERSION', version);
 constantModule.value('VERSIONS', {
   uchiwa: version
